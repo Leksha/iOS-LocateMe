c1f5650093f40181746ff2f97ed6203d05814aef
Leksha Ramdenee <leksha.ramdenee@tulip.io>
2016-10-03 16:59:38 -0400

    Added MapPoint.h and MapPoint.m

---
+++ LocateMe/MapPoint.h
@@ -0,0 +1,32 @@
+//
+//  MapPoint.h
+//  LocateMe
+//
+//  Created by Leksha Ramdenee on 2016-10-03.
+//  Copyright © 2016 Leksha Ramdenee. All rights reserved.
+//
+
+#ifndef MapPoint_h
+#define MapPoint_h
+
+#import <Foundation/Foundation.h>
+#import <CoreLocation/CoreLocation.h>
+#import <MapKit/MapKit.h>
+
+@interface MapPoint : NSObject <MKAnnotation>
+{
+    NSString *title;
+    CLLocationCoordinate2D coordinate;
+}
+
+// A new designated initializer for instances of MapPoint
+- (id)initWithCoordinate:(CLLocationCoordinate2D)c title:(NSString *)t;
+
+// This is a required property from MKAnnotation
+@property (nonatomic, readonly) CLLocationCoordinate2D coordinate;
+
+// This is an optional property from MKAnnotation
+@property (nonatomic, copy) NSString *title;
+
+@end
+#endif /* MapPoint_h */
---
+++ LocateMe/MapPoint.m
@@ -0,0 +1,26 @@
+//
+//  MapPoint.m
+//  LocateMe
+//
+//  Created by Leksha Ramdenee on 2016-10-03.
+//  Copyright © 2016 Leksha Ramdenee. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "MapPoint.h"
+
+@implementation MapPoint
+
+@synthesize coordinate, title;
+
+- (id)initWithCoordinate:(CLLocationCoordinate2D)c title:(NSString *)t
+{
+    self = [super init];
+    if (self){
+        coordinate = c;
+        [self setTitle:t];
+    }
+    return self;
+}
+
+@end
